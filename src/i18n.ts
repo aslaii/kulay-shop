a362851061c77b1a52ae46cdd3a7cceaec66289b
Jericho Bermas <jecho.deleon@gmail.com>
2026-02-24 21:36:59 +0800

    feat(i18n): setup i18next with expo-localization and en.json

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,4 +1,4 @@
-import i18next from 'i18next';
+import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import * as Localization from 'expo-localization';
 import en from './locales/en.json';
@@ -9,7 +9,7 @@ const resources = {
   },
 };
 
-i18next
+i18n
   .use(initReactI18next)
   .init({
     resources,
@@ -20,4 +20,4 @@ i18next
     },
   });
 
-export default i18next;
+export default i18n;
